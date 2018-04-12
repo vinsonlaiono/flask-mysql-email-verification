5d3dc2d224e28962290d3c18957fe6f97eb35f5a
vinson aiono <vinson.aiono@gmail.com>
2018-04-11 20:46:01 -0700

    added all validations

--- server.py
+++ server.py
@@ -18,34 +18,67 @@ def index():
 
     hello_hash = bcrypt.generate_password_hash('13005907Va')
     print(hello_hash)
-    
-
     return render_template('index.html')
+
+#
 @app.route('/process', methods=['POST', 'GET'])
 def process():
-
-    # checks if email is valid
     email = request.form['email']
+
+    # checks if email is valid---------------
     if len(email) < 1:
         flash("Email cannot be blank!")
         return redirect('/')
     elif not EMAIL_REGEX.match(request.form['email']):
         flash("Invalid Email Address!")
         return redirect('/')
-
-
-    # query to insert the email from the user input into the DB
+    # check is email is aleady in the database------------------
+    check = "SELECT email FROM email WHERE email = %(email)s"
+    email_stuff =  {'email': email}
+    email_check = mysql.query_db(check, email_stuff)
+    if email_check:
+        flash("Email already exits!")
+        return redirect('/')
+    # query to insert the email from the user input into the DB----------------
     query = "INSERT INTO email (email, created_at, updated_at) VALUES (%(email)s, NOW(), NOW());"
-    data = {
-             'email': request.form['email']
-           }
+    data = {'email': request.form['email']}
     mysql.query_db(query, data)
 
     # query for all emails from DB
-    all_emails = mysql.query_db("SELECT email FROM email")
+    all_emails = mysql.query_db("SELECT id, email, created_at FROM email")
     print("Fetched all friends", all_emails)
 
-    return render_template('results.html', email = all_emails)
+    return render_template('results.html', email = all_emails, name = email)
+
+@app.route('/results')
+def results():
+    all_emails = mysql.query_db("SELECT id, email, created_at FROM email")
+    return render_template("results.html", email = all_emails)
+# -------------DELETE EMAIL----------------
+@app.route("/delete/<id>")
+def delete(id):
+    print(id)
+    query = "DELETE FROM email WHERE id = %(id)s;"
+    data = {
+                'id': id
+           }
+    mysql.query_db(query, data)
+
+    all_emails = mysql.query_db("SELECT id, email, created_at FROM email")
+    # return render_template('results.html', email = all_emails)
+    return redirect('/results')
+
+# returns user to main page
+@app.route('/go_back')
+def goback():
+    return redirect('/')
+
+
+
+# @app.route('/delete')
+# def deleteEmail():
+#     deletquery = "DELETE FROM email WHERE id = %()"
+
 
 
 if __name__ == "__main__":
